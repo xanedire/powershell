77c90505fa183cd41d5d7f7492bbccba8028fea5
Geoffrey Erickson <44212221+xanedire@users.noreply.github.com>
2019-07-05 17:46:49 -0700

    Add files via upload

---
+++ sendmail_tolist.ps1
@@ -0,0 +1,13 @@
+﻿#CSV Format
+# LINE 1 User 1<user1@domain.org>
+# LINE 2 User 2<user2@domain.org>
+# LINE 3 User 3<user3@domain.org>
+
+$var=get-content U:\samplelist.csv
+$myemail = #'my.email@domain.com'
+$smtp= #"smpt.domain.com"
+[string[]]$to= $var
+$subject = read-host "enter subject"
+$body = read-host "enter message"
+
+send-mailmessage -to $to -subject $subject -body $body -smtpserver $smtp -from $myemail
