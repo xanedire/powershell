abad4441ccd202d0c5c9b6ecbab4d9d6aa3f2c70
xanedire <divemastergeoff@gmail.com>
2020-06-12 18:32:15 -0700

    added file to directory get-directory-and-permissions

---
+++ get-directory-and-permissions/get-directory-and-permissions.ps1
@@ -0,0 +1,17 @@
+﻿function get-directory-permissions{ #($input){
+    $folder = read-host 'input directory here'
+    (get-acl "$folder").access | ft IdentityReference,FileSystemRights,AccessControlType,IsInherited -auto
+    }
+function get-permission-description{ #($input){
+    $group = read-host 'enter group name'
+    Get-ADGroup -identity "$group" -Properties Description | Select-Object Description
+    }
+function start-script{
+    switch(read-host 'wat is needed? (1) permissions, (2) group, (3) exit'){
+        1{get-directory-permissions;pause;start-script}
+        2{get-permission-description;pause;start-script}
+        3{write-host "later dude";pause;exit}
+        default{write-host "say whaaaaaaat";start-script}
+    }
+}
+start-script
